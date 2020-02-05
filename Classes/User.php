397f50f0924e7456ba2f8fe81d92154c0ac5f742
Stevan <stevan.zdravkovic.309.16@ict.edu.rs>
2020-02-05 16:55:44 +0100

    search

--- Classes/User.php
+++ Classes/User.php
@@ -53,13 +53,13 @@ class User extends Database
     public function search($name_user, $category)
     {
         $sql = "SELECT * FROM users INNER JOIN categories ON users.category_id = categories.id_category 
-                WHERE users.name_user=:name_user AND (categories.id_category=:id_category OR categories.parent=:id_category OR categories.id_category IN 
+                WHERE users.name_user like :name_user AND (categories.id_category=:id_category OR categories.parent=:id_category OR categories.id_category IN
                 (SELECT c2.id_category FROM categories c INNER JOIN categories c2 ON c.id_category=c2.parent WHERE c.parent=:id_category OR categories.id_category IN
                 (SELECT c3.id_category FROM categories c4 INNER JOIN categories c3 ON c4.id_category=c3.parent WHERE c4.parent IN
                 (SELECT c5.parent FROM categories c6 INNER JOIN categories c5 ON c6.id_category=c5.parent WHERE c6.parent=:id_category))))
 ";
         $stmt = $this->connect()->prepare($sql);
-        $stmt->bindValue(":name_user", $name_user);
+        $stmt->bindValue(":name_user", $name_user.'%');
         $stmt->bindValue(":id_category", $category);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
